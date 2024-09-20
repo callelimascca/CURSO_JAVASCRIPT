046ed6fa67ded0648a67852b23f6b3fa3650a3e9
lourdes <132000301+callelimascca@users.noreply.github.com>
2024-09-20 08:27:47 -0500

    actualizacion

--- 1.Introduccion/comentarios.js
+++ 1.Introduccion/comentarios.js
@@ -2,3 +2,16 @@
 console.log("hola mundo")
 //para comentar codigo que no deseamos que se ejecute
 //console.log("hola mundo")
+/*comentarios multilinea*/
+
+/** doc screen
+ * sirve para documentar
+ * ejmpl
+  * funcion para sumar dos numeros
+  * recibe dos parametros
+  * retorna la suma de ambos
+**/
+function MediaKeyStatusMap(a,b){
+    return a+b
+}
+/*esta asociado a la funcion que esta despues del comentario, explica para que se usa la funcion*/
