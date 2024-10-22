54f69330413d96bb9f64484bb8c017bc795b1748
lourdes <132000301+callelimascca@users.noreply.github.com>
2024-10-22 09:17:09 -0500

    actualizacion

--- 3.Estructura_del _programa/Practica_Dos/ejercicioUno.js
+++ 3.Estructura_del _programa/Practica_Dos/ejercicioUno.js
@@ -1,3 +1,7 @@
-for (let rept = 1; rept <= 7; rept++) {
-    console.log('*'.repeat(rept));
+for (let linea = 1; linea <= 7; linea++) {
+    let estrellas = '';
+    for (let asterisco = 1; asterisco <= linea; asterisco++) {
+        estrellas += '*';
+    }
+    console.log(estrellas);
 }
