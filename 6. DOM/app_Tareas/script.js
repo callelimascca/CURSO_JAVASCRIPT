691a22603642a16b51125fb0260aae424112d651
callelimascca <callelimasccamaria13@gmail.com>
2024-12-04 13:29:06 -0500

    ejercicios de eventos de DOM

--- 6. DOM/app_Tareas/script.js
+++ 6. DOM/app_Tareas/script.js
@@ -1,13 +1,23 @@
-let input=document.querySelector("#tarea")
-let button=document.querySelector("#btn-agregar")
-let ul=document.querySelector("#lista-tareas")
-button.addEventListener("click",()=> {
-    // let li=document.createElement("li")
-    // li.textContent=input.value
-    // ul.appendChild(li)
-    ul.innerHTML+=
-    <li>${input.value}<button>eliminar</button></li> 
-    input.value=" "
-})
-let eliminar=document.querySelector()
-//agregar un boton eliminar por cada lista
+let input = document.querySelector("#tarea");
+let button = document.querySelector("#btn-agregar");
+let ul = document.querySelector("#lista-tareas");
+
+button.addEventListener("click", () => {
+
+    let li = document.createElement("li");
+    li.textContent = input.value;
+
+    let eliminarButton = document.createElement("button");
+    eliminarButton.textContent = "eliminar";
+
+    li.appendChild(eliminarButton);
+
+    ul.appendChild(li);
+
+    input.value = "";
+
+    eliminarButton.addEventListener("click", () => {
+
+        ul.removeChild(li);
+    });
+});
